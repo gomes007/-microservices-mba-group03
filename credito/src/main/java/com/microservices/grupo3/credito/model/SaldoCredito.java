8a883c5b725e48fc49d81a3e6ec6cb0ec2d89896
Paulo Gomes <73363753+gomes007@users.noreply.github.com>
2022-03-27 00:19:58 -0300

    Add files via upload

---
+++ credito/src/main/java/com/microservices/grupo3/credito/model/SaldoCredito.java
@@ -0,0 +1,22 @@
+package com.microservices.grupo3.credito.model;
+
+public class SaldoCredito {
+	
+	 private double saldoCredito;
+
+
+
+	    public SaldoCredito(double saldoCredito) {
+		super();
+		this.saldoCredito = saldoCredito;
+	}
+
+		public double getSaldoCredito() {
+	        return saldoCredito;
+	    }
+
+	    public void setSaldoCredito(double saldoCredito) {
+	        this.saldoCredito = saldoCredito;
+	    }
+
+}
